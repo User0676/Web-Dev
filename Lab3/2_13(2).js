fad2cb7f863692fa99645bdd0bea77f9253e165f
User0676 <japangod202203@gmail.com>
2023-02-15 01:48:42 +0600

    learned and solved until section: 16.Function Expressions

---
+++ Lab3/2_13(2).js
@@ -0,0 +1,7 @@
+let i = 0;
+while (++i < 5) alert( i );
+//1 until 4
+
+let j = 0;
+while (j++ < 5) alert( j );
+//1 until 5
---
+++ Lab3/2_15(1).js
@@ -0,0 +1,15 @@
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    }
+    // ...
+    return confirm('Did parents allow you?');
+  }
+
+  function checkAge2(age) {
+    if (age > 18) {
+      return true;
+    }
+    // ...
+    return confirm('Did parents allow you?');
+  } //same
