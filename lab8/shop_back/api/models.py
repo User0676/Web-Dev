56d374b653694a8f7c7a520cb15ccedec84ee081
User0676 <japangod202203@gmail.com>
2023-04-04 01:43:17 +0600

    added models

--- lab8/shop_back/api/models.py
+++ lab8/shop_back/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('name',max_length=50)
+    price = models.FloatField('price')
+    description = models.TextField('description')
+    count = models.IntegerField('count')
+    is_active = models.BooleanField('status')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Product'
+
+
+class Category(models.Model):
+    name = models.CharField('name',max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category'
+
